5fca7b5ea40b4dc682d43a9afdc3ed5f4d0ae36d
Twister10000 <mika.juon@gmail.com>
2024-09-30 15:16:40 +0200

    Update AES10_DATA_ENCODER.vhd
    Es wurde Kosmetische Arbeiten gemacht.

--- Source/AES10_DATA_ENCODER.vhd
+++ Source/AES10_DATA_ENCODER.vhd
@@ -30,11 +30,11 @@ architecture BEH_AES10_DATA_ENCODER of AES10_DATA_ENCODER is
 	constant	Sync_Symbol			:	std_logic_vector	(9 downto	0)	:= "1100010001"; -- Symbol J(11000) and Symbol K(10001)
 	
 	--Signal Declarations 
-	signal	Send_SYNC				:	std_logic	:= '0';
-	signal	MADI_DATA_5bit 	: std_logic_vector	(4 downto 0) := (others	=> '0'); -- MADI_DATA_5bit(4) last send Bit
-	signal	CTN							: integer range 0 to 16 :=	0;
-	signal	Word_CTN				:	integer	range 0 to 1024	:=	0;
-	signal	CTN_SYNC				: integer range 0 to 16 :=	0;
+	signal	Send_SYNC				:	std_logic												:= '0';
+	signal	MADI_DATA_5bit 	: std_logic_vector	(4 downto 0) 	:= (others	=> '0'); -- MADI_DATA_5bit(4) last send Bit
+	signal	CTN							: integer range 0 to 16 					:=	0;
+	signal	Word_CTN				:	integer	range 0 to 1024					:=	0;
+	signal	CTN_SYNC				: integer range 0 to 16 					:=	0;
 
 
 begin
@@ -88,9 +88,6 @@ begin
 						if CTN >= 4 then
 							CTN <= 0;
 							Word_CTN	<=	Word_CTN + 1;
-							if Word_CTN	>= 512 then
-								Send_SYNC	<= '1';
-							end if;
 						elsif CTN =	3	then
 						
 							FIFO_READ_ENA <= '1'; --FIFO Read_Enbaled active
