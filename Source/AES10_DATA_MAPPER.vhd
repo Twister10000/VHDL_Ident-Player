69c419bf4ab3112ce397c581dac0bf42a3b97377
Twister10000 <82016318+Twister10000@users.noreply.github.com>
2024-10-30 14:25:58 +0100

    Out of Range Bug Fixed

--- Source/AES10_DATA_MAPPER.vhd
+++ Source/AES10_DATA_MAPPER.vhd
@@ -228,7 +228,7 @@ begin
 										when 8 			to 15			=>	MADI_FRAME(25)	<=	BYTE1(MADI_BLOck_CTN - 8);
 										when 16 		to 23			=>	MADI_FRAME(25)	<=	BYTE2(MADI_BLOck_CTN - 16);
 										when 24 		to 31			=>	MADI_FRAME(25)	<=	BYTE3(MADI_BLOCK_CTN - 24);
-										when 32 		to 40			=>	MADI_FRAME(25)	<=	BYTE4(MADI_BLOCK_CTN - 32); 
+										when 32 		to 39			=>	MADI_FRAME(25)	<=	BYTE4(MADI_BLOCK_CTN - 32); 
 										when 184   	to 192		=>	MADI_FRAME(25)	<= 	BYTECRC(MADI_BLOCk_CTN - 184);
 										when others				=>	MADI_FRAME(25)	<= '0'; -- Channel Active 
  									end case;
