dd56bd9a0c7544cc9f24c137f693adf3d0a62c54
Twister10000 <82016318+Twister10000@users.noreply.github.com>
2024-11-02 15:43:33 +0100

    added SINE MIF fo Simulation

--- Source/ONCHIP_AUDIO_STORAGE/simulation/aldec/rivierapro_setup.tcl
+++ Source/ONCHIP_AUDIO_STORAGE/simulation/aldec/rivierapro_setup.tcl
@@ -12,7 +12,7 @@
 # or its authorized distributors. Please refer to the applicable 
 # agreement for further details.
 
-# ACDS 23.1 993 win32 2024.10.29.15:06:20
+# ACDS 23.1 993 win32 2024.11.02.15:43:12
 # ----------------------------------------
 # Auto-generated simulation script rivierapro_setup.tcl
 # ----------------------------------------
--- Source/ONCHIP_AUDIO_STORAGE/simulation/mentor/msim_setup.tcl
+++ Source/ONCHIP_AUDIO_STORAGE/simulation/mentor/msim_setup.tcl
@@ -94,7 +94,7 @@
 # within the Quartus project, and generate a unified
 # script which supports all the Altera IP within the design.
 # ----------------------------------------
-# ACDS 23.1 993 win32 2024.10.29.15:06:20
+# ACDS 23.1 993 win32 2024.11.02.15:43:12
 
 # ----------------------------------------
 # Initialize variables
