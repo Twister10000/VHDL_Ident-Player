a1b15b6b0dc357ca9ce3d95ec79db5d090b6c2d6
Twister10000 <82016318+Twister10000@users.noreply.github.com>
2024-11-28 11:08:19 +0100

    cleaning

--- Source/Ident_Player_TOP.vhd
+++ Source/Ident_Player_TOP.vhd
@@ -487,23 +487,19 @@ begin
 																				end if;
 						
 						when	sFIFO_wr					=>	
-																			--if dat_valid	=	'1' then
 																				if CTN_dat_block	>= 144 then -- Bei nicht genauen Werte nkommt es zu Verzeerrungen 
 																					CTN_dat_block		<=	0;
 																					FSM_SDCARD			<=	idle;
 																					FIFO_wrreq_TOP	<=	'0';
 																				else
 																					CTN_dat_block		<=	CTN_dat_block	+	3;
-																					FIFO_DATA_INPUT	<=	x"00" & std_logic_vector(dat_block(CTN_dat_block)) & std_logic_vector(dat_block(CTN_dat_block + 1)) & std_logic_vector(dat_block(CTN_dat_block + 2));
+																					FIFO_DATA_INPUT	<=	x"00" & std_logic_vector(dat_block(CTN_dat_block + 2)) & std_logic_vector(dat_block(CTN_dat_block + 1)) & std_logic_vector(dat_block(CTN_dat_block));
 																					Test_SD_DATA_1	<=	std_logic_vector(dat_block(CTN_dat_block));
 																					Test_SD_DATA_2	<=	std_logic_vector(dat_block(CTN_dat_block + 1));
 																					Test_SD_DATA_3	<=	std_logic_vector(dat_block(CTN_dat_block + 2));
 																					FIFO_wrreq_TOP	<=	'1';
 																				end if;
-																			--else
-																			--	FSM_SDCARD	<= idle;
-																			--end if;
-																				FIFO_DATA_INPUT	<=	x"00" & std_logic_vector(dat_block(CTN_dat_block + 2)) & std_logic_vector(dat_block(CTN_dat_block + 1)) & std_logic_vector(dat_block(CTN_dat_block));
+																				
 						
 						when others							=> FSM_SDCARD	<=	idle;
 					
